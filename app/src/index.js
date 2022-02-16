615f183ca8768484306540cdd1dd26b891d41381
Korbinian Kaiser <korbinian.kaiser@gmx.de>
2022-02-16 11:57:11 +0100

    minor name changes
    @at0mstr0m requested new class names for two html elements

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,14 @@
 
 function init() {
     console.log("### Starting MME Project ###");
+    const   el = document.createElement("div"),
+            list = document.getElementsByClassName("discussion")[0];
+    el.innerHTML = document.getElementById("comment-field-template").innerHTML;
+    el.getElementsByClassName("username")[0].innerText = "name";
+    el.getElementsByClassName("title")[0].innerText = "title";
+    el.getElementsByClassName("message")[0].innerText = "message";
+
+    list.appendChild(el);
 }
 
 init();
