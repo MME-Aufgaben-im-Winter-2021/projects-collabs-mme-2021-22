b37050f56d77841fe0a9e60cbd781e01cab6ab9a
korbi-hub <76163367+korbi-hub@users.noreply.github.com>
2022-03-11 11:34:23 +0100

    Revert "Merge branch 'frame_list_features' into dev"
    This reverts commit 35caf8d434c251b46ea76681446f0666c4755c17, reversing
    changes made to 9fb1911c245b4f8ea9bf1d3e8021f9238d785725.

--- app/src/ui/NavBarView.js
+++ app/src/ui/NavBarView.js
@@ -10,13 +10,10 @@ class NavBarView extends Observable {
         super();
         this.body = createElementFromHTML(document.querySelector("#navbar-template").innerHTML);
         this.userDisplayName = this.body.querySelector(".user-display-name");
+        this.userDisplayName.innerHTML = displayName;
         this.logoutButton = this.body.querySelector(".logout-button");
         this.logoutButton.addEventListener("click", this.onLogoutButtonClicked.bind(this));
-        this.loginButton = this.body.querySelector(".login-button");
-        this.loginButton.addEventListener("click", this.onLoginButtonClicked.bind(this))
-        this.homeScreenButton = this.body.querySelector(".home-screen");
-        this.homeScreenButton.addEventListener("click", this.displayHomeScreen.bind(this));
-        this.newProjectButton = this.body.querySelector(".new-project");
+        this.projectsDropdownMenuView = new ProjectsDropdownMenuView(this.body);
         this.projectsDropdownMenuView.addEventListener("projectSelected", this.onProjectSelected.bind(this));
     }
 
@@ -34,22 +31,6 @@ class NavBarView extends Observable {
 
     onProjectSelected(event) {
         this.notifyAll(new Event("projectSelected", { id: event.data.id }));
-    }
-
-    toggleUIVisibility(makeVisible) {
-        if (makeVisible) {
-            this.projectsDropdownMenuView.dropdownButton.style.display = "flex";
-            this.userDisplayName.style.display = "block";
-            this.logoutButton.style.display = "block";
-            this.newProject.style.display = "block";
-            this.loginButton.style.display = "none";
-        } else {
-            this.projectsDropdownMenuView.dropdownButton.style.display = "none";
-            this.userDisplayName.style.display = "none";
-            this.logoutButton.style.display = "none";
-            this.newProject.style.display = "none";
-            this.loginButton.style.display = "block";
-        }
     }
 }
 
